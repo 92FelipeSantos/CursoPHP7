bff4f3018074d7e768af0360cc8385036a71b512
Felipe Santos <felipesantos.992@gmail.com>
2018-09-18 22:19:25 -0300

    Adiciona mais arquivos na pasta variaveis

---
+++ cursophp7/variaveis/exemplo-05.php
@@ -0,0 +1,16 @@
+<?php
+
+  $nome = "Felipe Santos";
+
+  function teste(){
+    global $nome;
+    echo $nome;
+  }
+
+  function teste2(){
+  	echo $nome." agora no teste 2";
+  }
+
+  teste();
+  teste2();
+?>
