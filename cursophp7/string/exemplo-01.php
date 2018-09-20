a11ea80f7ae7d44c8f4ffff108416e594b09a6cf
Felipe Santos <felipesantos.992@gmail.com>
2018-09-19 21:42:37 -0300

    Inclusão de pastas e arquivos do curso

---
+++ cursophp7/string/exemplo-01.php
@@ -0,0 +1,14 @@
+<?php
+
+    $nome = "felipe santos";
+    echo strtoupper($nome);
+    echo "<br>";
+    echo strtolower($nome);
+    echo "<br>";
+    
+    echo ucfirst($nome);
+    echo "<br>";
+    echo ucwords($nome);
+    echo "<br>";
+
+?>
---
+++ cursophp7/string/exemplo-02.php
@@ -0,0 +1,6 @@
+<?php 
+
+	$empresa = "HCode";
+	echo str_replace("o", "0", $empresa);
+
+?>
